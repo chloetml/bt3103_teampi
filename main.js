9771de82843c8e980a9902684dcb3bc3277818a6
chloetml <42717304+chloetml@users.noreply.github.com>
2018-11-13 23:46:39 +0800

    work

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ var app = new Vue({
     hangouts: "",
     currUserRef: "",
     userName: "",
-    userPassword: ""
+    userPassword: "",
+    studName: "name",
+    faculty: "faculty"
   },
   methods: {
     get: function() {
@@ -36,6 +38,49 @@ var app = new Vue({
       this.hangouts = arr;
       return arr;
     },
+    evalFac: function(fac) { // function to evaluate faculty value
+      var text = "";
+      switch (fac) {
+        case "BIZ":
+          text = "School of Business";
+          break;
+        case "FoE":
+          text = "Faculty of Engineering";
+          break;
+        case "FASS":
+          text = "Faculty of Arts & Social Sciences";
+          break;
+        case "FoS":
+          text = "Faculty of Science";
+          break;
+        case "SDE":
+          text = "School of Design & Environment";
+          break;
+        case "NUSMed":
+          text = "School of Medicine";
+          break;
+        case "SoC":
+          text = "School of Computing";
+          break;
+        case "SPP":
+          text = "School of Public Policy";
+          break;
+        case "FoD":
+          text = "Faculty of Dentistry";
+          break;
+        case "MUS":
+          text = "Yong Siew Toh Conservatory of Music";
+          break;
+        default:
+          text = "Faculty";
+      }
+      return text;
+    },
+    /*
+      array(['BIZ', 'FoE', 'FASS', 'FoS', 'SDE', 'SCALE', 'NUS', 'SMA', 'MDP',
+          'NUSMed', 'ISS', 'RMI', 'FoL', 'SoC', 'SPP', 'NGS', 'FoD', 'SPH',
+          'MUS', 'YNC', 'DUKE MS', 'TLIAP', 'TDSI'], dtype=object)
+    */
     verifyUserNP: function() {
       var contains = 0;
       var id = this.userName;
@@ -46,10 +91,13 @@ var app = new Vue({
           var userID = userSnapshot.child("id").val();
           var pass = userSnapshot.child("password").val();
           if (userID === id && pass === pw) {
+            console.log(userSnapshot.key);
             contains = 1;
             //console.log(userSnapshot.ref());
             console.log(contains);
             //this.currUserRef = userSnapshot.key;
+          this.studName = userSnapshot.child("name").val();
+          this.faculty = this.evalFac(userSnapshot.child("faculty").val());
           }
         });
         //console.log(userArr);
