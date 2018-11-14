392858eccffedbb7a4e8a1a689bd5752708e0849
chloetml <42717304+chloetml@users.noreply.github.com>
2018-11-15 02:49:23 +0800

    u

--- booking.js
+++ booking.js
@@ -48,17 +48,60 @@ var app = new Vue({
       var currRef = this.currUserRef;
       window.location.href = "/bt3103_teampi/home.html?currRef=" + currRef + "";
     },
+    formatTime: function(hour, merid) {
+      //formatting time to 24h format
+      var hNum;
+      var h = "0";
+      var time = "";
+      if (merid === "AM") {
+        //morning time: 9AM -11AM
+        if (hour === 9) {
+          h = h + hour;
+        } else {
+          // hour can be 10 or 11
+          time = hour + "00";
+        }
+      } else if (merid === "PM") {
+        //time can be 12, 1, 2...10PM
+        if (hour === 12) {
+          time = hour + "00";
+        } else {
+          hNum = hour + 12;
+          time = hNum + "00";
+        }
+      }
+      return time;
+    },
+    formatDate: function(date) {
+      var formattedDate, dS, mS;
+      var d = date.getDate(); //1 to 31
+      var m = date.getMonth() + 1; //1 to 12
+      var y = d.getFullYear();
+      if (0 < d < 10) {
+        dS = "0" + d;
+      } else if (d > 9) {
+        dS = "" + d;
+      }
+      if (0 < m < 10) {
+        mS = "0" + m;
+      } else if (m > 9) {
+        mS = "" + m;
+      }
+      formattedDate = dS + mS + y;
+      return formattedDate;
+    },
     goSearch: function() {
       var currRef = this.currUserRef;
       var date = $("#datepicker").datepicker("getDate");
       var hours = $("#tp").data("timepicker").hour;
-      console.log(hours);
+      //console.log(hours); //returns single digit
       var meridian = $("#tp").data("timepicker").meridian;
+      var time = this.formatTime(hours, meridian);
       var venueType = this.venueType;
       console.log(date);
       console.log(venueType);
-      var m = moment(date);
-      var stringDate = m.format("DDMMYYYY");
+      var stringDate = this.formatDate(date);
+
       if (venueType === "") {
         alert("Please select a type of venue.");
       } else {
